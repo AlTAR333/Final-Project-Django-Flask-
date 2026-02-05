1bb14f72977a55433ab7fe0a6fd2bb31a6770e3a
Armand <armand.vieau@epita.fr>
2026-02-06 00:08:39 +0100

    Play and PlaySession models for stats and path track

--- django_web/stories/models.py
+++ django_web/stories/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Play(models.Model):
+    story_id = models.IntegerField()
+    ending_page_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Play story {self.story_id} -> ending {self.ending_page_id}"
+
+class PlaySession(models.Model):
+    story_id = models.IntegerField()
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(null=True, blank=True)  # set when story ends
+
+    def is_finished(self):
+        return self.ended_at is not None
