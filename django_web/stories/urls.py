983de082a535baba801efcaa898ac144e81c6a25
Armand <armand.vieau@epita.fr>
2026-02-06 00:08:05 +0100

    Urls for stories + stats

--- django_web/stories/urls.py
+++ django_web/stories/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, api_test
+from . import views
 
 urlpatterns = [
-    path("", home),
-    path("api-test/", api_test),
+    path("api-test/", views.api_test),
+    path("", views.story_list, name="story_list"),
+    path("play/<int:story_id>/", views.play_story, name="play_story"),
+    path("play/<int:story_id>/<int:page_id>/", views.play_story, name="play_story"),
+    path("stats/", views.stats, name="stats"),
 ]
