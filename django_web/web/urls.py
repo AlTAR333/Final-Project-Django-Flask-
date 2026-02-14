c1d95a2378c0c6d3c0539d3ad571d37ac79e3444
Armand <armand.vieau@epita.fr>
2026-02-14 15:39:59 +0100

    Login and logout urls

--- django_web/web/urls.py
+++ django_web/web/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from stories import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path("", auth_views.LoginView.as_view(template_name="gameplay/login.html"), name="login"), #login as root, could be changed
+    path("login/", auth_views.LoginView.as_view(template_name="gameplay/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
     path("", include("stories.urls")),
 ]
