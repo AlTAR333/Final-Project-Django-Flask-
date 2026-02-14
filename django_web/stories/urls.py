8340a39eb7abdafe6c1f7fe02f77b85546e55976
Armand <armand.vieau@epita.fr>
2026-02-14 15:39:43 +0100

    author, stats and register url added

--- django_web/stories/urls.py
+++ django_web/stories/urls.py
@@ -3,8 +3,16 @@ from . import views
 
 urlpatterns = [
     path("api-test/", views.api_test),
-    path("", views.story_list, name="story_list"),
+
+    path("stories/", views.story_list, name="story_list"),
     path("play/<int:story_id>/", views.play_story, name="play_story"),
     path("play/<int:story_id>/<int:page_id>/", views.play_story, name="play_story"),
+    
     path("stats/", views.stats, name="stats"),
+    
+    path("author/", views.author_stories, name="author_stories"),
+    path("author/story/<int:story_id>/preview/", views.preview_story, name="preview_story"),
+    path("author/story/<int:story_id>/delete/", views.delete_story, name="delete_story"),
+    
+    path("register/", views.register, name="register")
 ]
